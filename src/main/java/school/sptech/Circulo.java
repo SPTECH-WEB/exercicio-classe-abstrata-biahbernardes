d4e251f70c0a0488338edc0668cb4e3b76474ee7
biahbernardes <beatriz.dsilva@sptech.school>
2025-10-31 13:19:36 -0300

    finalizado - Beatriz 04251122

--- src/main/java/school/sptech/Circulo.java
+++ src/main/java/school/sptech/Circulo.java
@@ -1,4 +1,30 @@
 package school.sptech;
 
-public class Circulo {
+public class Circulo extends Figura {
+    private Double raio;
+
+    public Circulo() {}
+
+    public Circulo(Double raio) {
+        this.raio = raio;
+    }
+
+    public Double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(Double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public Double calcularArea() {
+        return Math.PI * (raio * raio);
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo:" + "Raio:" + raio + "Área:" + calcularArea();
+    }
 }
+
--- src/main/java/school/sptech/Quadrado.java
+++ src/main/java/school/sptech/Quadrado.java
@@ -1,4 +1,30 @@
 package school.sptech;
 
-public class Quadrado {
+public class Quadrado extends Figura {
+    private Double lado;
+
+    public Quadrado() {}
+
+    public Quadrado(Double lado) {
+        this.lado = lado;
+    }
+
+    public Double getLado() {
+        return lado;
+    }
+
+    public void setLado(Double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public Double calcularArea() {
+        return lado * lado;
+    }
+
+    @Override
+    public String toString() {
+        return "Quadrado:" + "Lado:" + lado + "Área:" + calcularArea();
+    }
 }
+
